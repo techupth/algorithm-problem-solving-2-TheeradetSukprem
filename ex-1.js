9283feda06dadb96648b56e484f598c77a5a7490
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-17 15:01:22 +0700

    feat:complete algo 2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,39 @@
+students = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+
+// function findStudentIndex(students, searchStudent) {
+// 	let index = -1
+// 	for (let i = 0; i < students.length; i++) {
+// 		console.log(students[i]);
+// 		if (students[i] === searchStudent) {
+// 			index = i;
+// 		}
+// 	}
+// 	return index
+// }
+
+// console.log(findStudentIndex(students,"Andrew"));
+
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+let left = 0 ;
+let right = students.length - 1 ; 
+let index = -1 ;
+
+	while (left <= right ) {
+		
+	let mid = Math.floor ((left + right) / 2 ) ;
+	if (students[mid] === searchStudent ) {
+		index = mid ;
+		break;
+	} else if (students[mid] < searchStudent){
+		left = mid + 1
+	} else {
+		right = mid - 1
+	}
+	}
+	return index
 }
 
-// ตอบคำถามตรงนี้จ้า
+console.log(findStudentIndex(students,"Andrew"));
+console.log(findStudentIndex(students,"Andre"));
+
+// ตอบคำถามตรงนี้จ้า O log n หรือเปล่าครับเพราะว่าเราหาแบบถ้าไม่ใช่ให้ขยับไปเรื่อยๆแต่ต่างจาก O n ที่ไล่ที่ละตัวส่วน O^n ส่วน O log n นั้นตัดจำนวนที่ค้องหาคล้ายๆการหารหรือตัดออก
